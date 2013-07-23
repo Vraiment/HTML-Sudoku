b7be878f6f0b092c7361d90fa68b933269c81031
Vraiment <jemc44@gmail.com>
2013-07-23 17:30:39 -0500

    save a load functions

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple HTML app for play Sudoku.
+
+It requires JQuery and [JQuery Numeric](https://github.com/SamWM/jQuery-Plugins/tree/master/numeric/).
+
+Only tested in Google Chrome.
--- sudoku.js
+++ sudoku.js
@@ -6,8 +6,50 @@ function foreach(callback) {
 	}
 }
 
+function setValue(input, set) {
+	if (set) {
+		input.addClass('value');
+		input.attr('readonly', true);
+	} else {
+		input.removeClass('value');
+		input.attr('readonly', false);
+	}
+}
+
+function load() {
+	var data = $('#sudokuData').val();
+	
+	foreach(function(input, i, j) {
+		var position = (i * 18) + (j * 2);
+		
+		setValue(input, data.charAt(position) == '1' && !$('#enableEdit').is(':checked'));
+		
+		var value = data.charAt(position + 1);
+		
+		if (value != '0') {
+			input.val(value);
+		}
+	});
+}
+
+function save() {
+	var result = '';
+	
+	foreach(function(input) {
+		var current = 0;
+		
+		result += (input.hasClass('value')) ? 1 : 0;
+		result += (input.val()) ? input.val() : 0;
+	});
+	
+	$('#sudokuData').val(result);
+}
+
 $(document).ready(function() {
 	$('.sqr').numeric();
+	
+	$('#saveData').click(save);
+	$('#loadData').click(load);
 	
 	$('#enableEdit').attr('checked', true);
 	
